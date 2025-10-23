aef8eb8f61052aabbaa22aa35b5083cff16eb030
andresmend12-lab <andresmend12@gmail.com>
2025-10-23 02:22:24 +0200

    cambio

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.7.2" apply false
-    kotlin("android") version "1.9.22" apply false
+    kotlin("android") version "1.9.23" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.18" apply false
 }
 
