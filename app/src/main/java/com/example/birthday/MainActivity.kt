38fb1c5b67b2b8d5a583b81f317335e5af0c9e61
andresmend12-lab <andresmend12@gmail.com>
2025-10-23 09:47:37 +0200

    Fix MainActivity onNewIntent signature

--- app/src/main/java/com/example/birthday/MainActivity.kt
+++ app/src/main/java/com/example/birthday/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : ComponentActivity() {
         handleNavigationIntent(intent)
     }
 
-    override fun onNewIntent(intent: Intent?) {
+    override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         setIntent(intent)
         handleNavigationIntent(intent)
